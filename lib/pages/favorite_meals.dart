c8a63f80557541e1965b0866923716b366096f1e
Anisul Islam <air.rohit999@gmail.com>
2019-10-15 23:26:27 +0600

    Displaying favorite meals

--- lib/pages/favorite_meals.dart
+++ lib/pages/favorite_meals.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 import '../models/Meal.dart';
 
+import '../widgets/meal_item.dart';
+
 class FavoriteMealsPage extends StatelessWidget {
   final List<Meal> favoriteMeals;
 
@@ -9,6 +11,30 @@ class FavoriteMealsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    if (favoriteMeals.isEmpty) {
+      return Center(
+        child: Text(
+          "You have no favorite meal yet ..!!",
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      );
+    } else {
+      return ListView.builder(
+        itemBuilder: (ctx, index) {
+          return MealItem(
+            id: favoriteMeals[index].id,
+            title: favoriteMeals[index].title,
+            imageUrl: favoriteMeals[index].imageUrl,
+            duration: favoriteMeals[index].duration,
+            affordability: favoriteMeals[index].affordability,
+            complexity: favoriteMeals[index].complexity,
+          );
+        },
+        itemCount: favoriteMeals.length,
+      );
+    }
   }
 }
